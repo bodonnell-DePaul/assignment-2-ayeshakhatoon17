279180d052eda4fd850a065bff53bdda8c388c0c
ayesha <fayeshak@depaul.edu>
2024-10-09 15:26:02 +0000

    todo

--- todo-list/src/todoItems.js
+++ todo-list/src/todoItems.js
@@ -2,17 +2,17 @@ const todoItems = [
     {
       title: "Finish Assignment",
       description: "Complete the React Bootstrap ToDo List Assignment.",
-      dueDate: new Date().toISOString().split("T")[0], // today's date
+      dueDate: new Date().toISOString().split("T")[0], 
     },
     {
       title: "Go Grocery Shopping",
       description: "Buy groceries for the week.",
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 5)).toISOString().split("T")[0], // 5 days from now
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 5)).toISOString().split("T")[0], 
     },
     {
       title: "Pay Rent",
       description: "Pay rent for the month.",
-      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)).toISOString().split("T")[0], // 2 days from now
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 2)).toISOString().split("T")[0], 
     },
   ];
   
